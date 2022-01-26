2deffc9c9ae60fd00d6c2bb5c6dfcf14fd90e10e
Robert <robert_025@mail.ru>
2022-01-26 14:56:49 +0600

    Правки по замечаниям на защите проекта

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,7 +1,11 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var logo = document.querySelector('.logo');
 
+logo.classList.remove('page-header__logo--no-js');
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
